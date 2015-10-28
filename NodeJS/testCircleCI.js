0d4afa4752efe2754db7385a4574891e2b47ba98
violinbeats <vincentsu.violin@gmail.com>
2015-10-27 21:03:11 -0700

    added first test using mocha

--- NodeJS/testCircleCI.js
+++ NodeJS/testCircleCI.js
@@ -1,1 +1,6 @@
-console.log('I am a sample Node.js program!')
+var assert = require('assert');
+describe('String#split', function(){
+	  it('should return an array', function(){
+		      assert(Array.isArray('a,b,c'.split(',')));
+			    });
+})
