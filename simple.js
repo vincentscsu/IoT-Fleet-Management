504731c26657731d20931f07d7115e87ad8a3cab
violinbeats <vincentsu.violin@gmail.com>
2015-11-25 12:12:04 -0800

    change port to 80 for public access

--- simple.js
+++ simple.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+PORT = 80;
+
 var str = 'Hello from Black Pearl!';
 
 app.get('/', function (req, res) {
   res.send(str);
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 
